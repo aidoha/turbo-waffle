585e3f58445353aae6c83981c4d30b5707655f73
AIbrayev <aibrayev@one.kz>
2021-05-31 14:40:49 +0600

    feat: add caching for request

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,51 +1,71 @@
-import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+  name: "fileCache",
+});
+
+(async () => {
+  await fileCache.setItem("color", "red");
+  const color = await fileCache.getItem("color");
+  console.log(color);
+})();
 
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
         }
 
-        if (args.path.includes('./') || args.path.includes('../')) {
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            namespace: 'a',
+            namespace: "a",
             path: new URL(
               args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
+              "https://unpkg.com" + args.resolveDir + "/"
             ).href,
           };
         }
 
         return {
-          namespace: 'a',
+          namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+        console.log("onLoad", args);
 
-        if (args.path === 'index.js') {
+        if (args.path === "index.js") {
           return {
-            loader: 'jsx',
+            loader: "jsx",
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              import React from 'react';
+              console.log(React);
             `,
           };
         }
 
+        const cachedResult = await fileCache.getItem(args.path);
+
+        if (cachedResult) {
+          return cachedResult;
+        }
         const { data, request } = await axios.get(args.path);
-        return {
-          loader: 'jsx',
+        const result = {
+          loader: "jsx",
           contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
